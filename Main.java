b3c82f94b883868af717c2c841c84d0aa5bc1019
dclliu <dclliu@gmail.com>
2017-03-22 12:21:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GOLGraphics gol = new GOLGraphics();
+		createGrid();
 	}
-
+	
+	
+	static void createGrid() {
+		JFrame f = new JFrame();
+        DrawHexagons p = new DrawHexagons();
+        f.setContentPane(p);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+	}
 }
