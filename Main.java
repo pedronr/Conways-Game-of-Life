2a5b9f858d82edc1fb10b8b757f8ebdf57faa80d
dclliu <dclliu@gmail.com>
2017-05-04 14:09:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,83 +13,128 @@ public class Main{
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.pack();
         f.setLocationRelativeTo(null);
+       // Point origin = new Point(p.getWidth(),p.getHeight());
+       // Hexagon[][] grid = p.makeHexGrid(origin, 10, 40, 0);
+        //grid[5][5].toggle();
         f.setVisible(true);
         Point origin = new Point(p.getWidth(),p.getHeight());
-        Hexagon[][] grid = p.makeHexGrid(origin, 10, 40, 0);
+        Hexagon[][] grid = p.getHexGrid();
         
         for(int i = 0; i < grid.length; i++) {
         	for(int j = 0; j < grid[0].length; j++) {
-        		System.out.print("("+grid[i][j].getCenter().getX()+", "+
-        	grid[i][j].getCenter().getY()+")");
+        		//System.out.print("("+grid[i][j].getCenter().getX()+", "+
+        	//grid[i][j].getCenter().getY()+")");
         	}
         	System.out.println();
         }
-        //grid[5][0].changeColor((Graphics2D)f.getGraphics(), 0);
+        //grid[14][0].toggle();
+        //grid[0][7].toggle();
+        //grid[0][6].toggle();
+        grid[7][4].toggle();
+        grid[7][5].toggle();
+        grid[6][5].toggle();
+        int i = 7;
+        int j = 4;
+        
+      //  grid[i][j-1].toggle();
+     //   grid[i][j+1].toggle();
+      //  grid[i-1][j].toggle();
+     //   grid[i+1][j].toggle();
+     //   grid[i-1][j-1].toggle();
+     //   grid[i+1][j-1].toggle();
+    //    grid[i][j].toggle();
+        for(int k = 0; k < 1000; k++) {
+        	iterate(p);
+        }
+       // grid[7][3].toggle();
+       // grid[14][1].toggle();
+       // grid[14][7].toggle();
+        //grid[6][14].toggle();
+        //grid[5][6].toggle();
 	}
 
 	static void iterate(DrawHexagons p) {
+		try{
+			Thread.sleep(1000);
+			//System.out.println("sleep");
+			}catch(InterruptedException ex) {
+				System.out.println("no");
+			}
 		int size = p.Size();
-		int half = size / 2;
 		boolean[][] newTurnedOn = new boolean[size][size];
-		Point origin = new Point(p.getWidth(),p.getHeight());
-        Hexagon[][] hexGrid = p.makeHexGrid(origin, 10, 40, 0);
-		for(int row = 0; row < 9; row++) {
-			int cols = size - java.lang.Math.abs(row - half);
-			for(int col = 0; col < cols; col++) {
-				int x = row < half ? col - row : col - half; //x val in array = col
-	            int y = row - half; //y val in array = row
-	            if(hexGrid[x][y] == null) {
-	            	break;
+        Hexagon[][] hexGrid = p.getHexGrid();
+		for(int i = 0; i < size; i++) {
+
+			for(int j = 0; j < size; j++) {
+
+	            if(hexGrid[i][j].getRadius()==0) {
+	            	//System.out.println("radius 0");
 	            }
-	            else if(x==0||x==9||y==0||y==9||x==5-y||x+y==14) {
+	            else if(i==0||i==(size-1)||j==0||j==(size-1)||i+j==size/2||i+j==size+size/2) {
 	            	//this checks borders
+	            	//System.out.println("border");
 	            }
 	            else {
+	            	//System.out.println("hello");
 	            	int count = 0;
-	            	if(hexGrid[x][y-1].isOn()) {
+	            	if(hexGrid[i][j-1].isOn()) {
 	            		count++;
 	            	}
-	            	if(hexGrid[x][y+11].isOn()) {
+	            	if(hexGrid[i][j+1].isOn()) {
 	            		count++;
 	            	}
-	            	if(hexGrid[x-1][y].isOn()) {
+	            	if(hexGrid[i-1][j].isOn()) {
 	            		count++;
 		            }
-	            	if(hexGrid[x+1][y].isOn()) {
+	            	if(hexGrid[i+1][j].isOn()) {
 	            		count++;
 		            }
-	            	if(hexGrid[x+1][y-1].isOn()) {
+	            	if(hexGrid[i-1][j-1].isOn()) {
 	            		count++;
 	            	}
-	            	if(hexGrid[x-1][y+1].isOn()) {
+	            	if(hexGrid[i+1][j+1].isOn()) {
 	            		count++;
 	            	}
 	            	//rules implemented below
-	            	if(hexGrid[x][y].isOn()) {
+	            	//System.out.println(count);
+	            	if(hexGrid[i][j].isOn()) {
+	            		
 	            		if(!(count==2)&&!(count==3)) {
-	            			newTurnedOn[x][y]=false;
+	            			newTurnedOn[i][j]=false;
+	            			//System.out.println("something been turned off");
 	            		}
 	            	}
-	            	else if(!hexGrid[x][y].isOn()) {
-	            		if(count==2||count==3)  {
-	            			newTurnedOn[x][y]=true;
+	            	else if(!hexGrid[i][j].isOn()) {
+	            		if(count==2)  {
+	            			newTurnedOn[i][j]=true;
+	            			//System.out.println(p.getHexGrid()[i][j].getCenter());
+	            			//System.out.println("Hexagon "+i+","+j+" been turn on");
 	            		}
 	            	}
 	            }
 			}
 		}
 		//copy newTurnedOn to hexGrid
-		for(int row = 0; row < 9; row++) {
-			int cols = size - java.lang.Math.abs(row - half);
-			for(int col = 0; col < cols; col++) {
-				int x = row < half ? col - row : col - half; //x val in array = col
-	            int y = row - half; //y val in array = row
-	            if(hexGrid[x][y].isOn()!=newTurnedOn[x][y]) {
-	            	hexGrid[x][y].toggle();
-	            }
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				if(hexGrid[i][j].getRadius()!=0) {
+					if(hexGrid[i][j].isOn()!=newTurnedOn[i][j]) {
+	            		hexGrid[i][j].toggle();
+	            		//System.out.println("Hexagon at "+i+", "+j+" has been toggled");
+	            	}
+				}
 			}
 		}
+		
 		//color hexagons here:
 		//
+		//for(int i = 0; i < 10; i++) {
+		//	for(int j = 0; j < 10; j++) {
+		//		if(hexGrid[i][j].isOn()) {
+		//			System.out.println("Hexagon at "+i+", "+j+" is on");
+		//		}
+		//	}
+		//}
+		p.repaint();
 	}
 }
