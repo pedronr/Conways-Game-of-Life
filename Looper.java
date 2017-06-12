b90033ca719ab95beb8c8fb23daed501633f3f0b
pedronr <peter@rowleyhome.com>
2017-06-12 15:20:53 -0400

    Update Looper.java

--- Looper.java
+++ Looper.java
@@ -3,16 +3,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.*;
 
 public class Looper implements Runnable {
-
+	
+	/* The purpose of this class is to use multi-threaded programming to be able to create the loop for the animation between pressing "start" and "end".
+	 * This class largely comes from the Internet as I do not know about volatile variables and multi-threaded programming.
+	 */
     private AtomicBoolean keepRunning;
     private Buttons[][] grid;
     private JFrame f;
     RealDrawing iterator = new RealDrawing();
-
+    
+    //Default, no-argument constructor.
     public Looper() {
         keepRunning = new AtomicBoolean(true);
     }
     
+    //This constructor is the one that is used in practice as it is the one that takes in the actual values for the grid and the JFrame.
     public Looper(Buttons[][] grid1, JFrame f1) {
     	grid = grid1;
     	f = f1;
