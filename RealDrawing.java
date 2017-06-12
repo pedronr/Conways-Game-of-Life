d2887537b462b860ffd3053e010f3550cd211f89
pedronr <peter@rowleyhome.com>
2017-06-12 15:20:27 -0400

    Update RealDrawing.java

--- RealDrawing.java
+++ RealDrawing.java
@@ -26,7 +26,7 @@ public class RealDrawing extends JPanel{
 		makeHexGrid(new Point(500,500), size, radius, 0);
 	}
 	
-	public void makeHexGrid(Point origin,int size, int radius, int padding){
+	public void makeHexGrid(Point origin,int size, int radius, int padding){ //not used
 		double ang30 = Math.toRadians(30);
         double xOff = Math.cos(ang30) * (radius + padding);
         double yOff = Math.sin(ang30) * (radius + padding);
@@ -41,46 +41,13 @@ public class RealDrawing extends JPanel{
                 int x = (int) (origin.x + xOff * (col * 2 + 1 - cols));
                 int y = (int) (origin.y + yOff * (row - half) * 3);
                 hexGrid[xLbl+half][yLbl+half] = new Buttons(new Hexagon(x,y,radius));
-                //System.out.println(hexGrid[xLbl+5][yLbl+5].getCenter().getX());
             }
         }
-     /*   for(int i = 0; i < size; i++) {
-        	for(int j = 0; j < size; j++) {
-        		if(hexGrid[i][j]==null) {
-        			hexGrid[i][j] = new Buttons((new Hexagon(new Point(0,0),radius)));
-        		}
-        	}
-        }*/
-		
-		/* 	double ang30 = Math.toRadians(30);
-        double xOff = Math.cos(ang30) * (radius + padding);
-        double yOff = Math.sin(ang30) * (radius + padding);
-        int half = size / 2;
-        for (int row = 0; row < size; row++) {
-            int cols = size - java.lang.Math.abs(row - half);
-            for (int col = 0; col < cols; col++) {
-                int xLbl = row < half ? col - row : col - half;
-                int yLbl = row - half;
-                int x = (int) (origin.x + xOff * (col * 2 + 1 - cols));
-                int y = (int) (origin.y + yOff * (row - half) * 3);
-                hexGrid[row][col] = new Buttons(new Hexagon(x,y,radius));
-                System.out.println("Created (" + row + "," + col + ") at (" + x + "," + y + ")");
-                //System.out.println(hexGrid[xLbl+5][yLbl+5].getCenter().getX());
-            }
-        }
-        for(int i = 0; i < size; i++) {
-        	for(int j = 0; j < size; j++) {
-        		if(hexGrid[i][j]==null) {
-        			hexGrid[i][j] = new Buttons((new Hexagon(new Point(40,40),radius)));
-        			System.out.println("There");
-        		}
-        	}
-        }*/
+     
 	}
 	
 	@Override
-	protected void paintComponent(Graphics g) {
-//		super.paintComponent(g);
+	protected void paintComponent(Graphics g) { //not used
 		Hexagon hexago = null;
 		Graphics2D g2 = (Graphics2D) g;
 		for (int i = 0; i < size; i++){
@@ -103,7 +70,8 @@ public class RealDrawing extends JPanel{
 		return hexGrid;
 	}
 	
-	public static void addListener(Buttons button1, JFrame f) {
+	public static void addListener(Buttons button1, JFrame f) { // A function that adds a MouseListener to a Buttons-type object that 
+																// toggles its color upon clicking or scrolling over with alt held down.
 		button1.addMouseListener(new MouseListener() {
 			@Override
     		public void mouseClicked(MouseEvent e) {
@@ -140,17 +108,18 @@ public class RealDrawing extends JPanel{
     		}
 		});
 
-    	//System.out.println("Added to grid[" + i + "][" + j + "]");
 	}
 	
 	public static void main(String[] args) {
 		
-		Buttons[][] grid = new Buttons[size][size];
+		Buttons[][] grid = new Buttons[size][size]; // the grid containing all the Buttons-type objects
 		double ang30 = Math.toRadians(30);
         double xOff = Math.cos(ang30) * (radius + 2);
         double yOff = Math.sin(ang30) * (radius + 2 );
         int half = size / 2;
 		
+        //To create the JFrame and JPanel that will contain the graphics
+        
         JFrame f = new JFrame();
         JPanel p = new JPanel();
         f.setContentPane(p);
@@ -160,7 +129,9 @@ public class RealDrawing extends JPanel{
         p.setLayout(null);
         p.setSize(500,500);
         p.setBackground(new Color(0x006606));
-         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+         //To add the correct coordinates to all of the Buttons objects in grid[][]
 
         Point origin = new Point((int)f.getSize().getWidth() / 2,(int)f.getSize().getHeight() / 2 - 30);
         int maxY = 0;
@@ -177,10 +148,10 @@ public class RealDrawing extends JPanel{
                 }
                 addListener(grid[xLbl+half][yLbl+half],f);
                 f.add(grid[xLbl+half][yLbl+half]);
-                //System.out.println((xLbl+half)+","+(yLbl+half));
             }
         }
         
+        //Creating the next, start, and end buttons, as well as the slider to adjust the speed of the animation.
         
           JButton nextButton = new JButton();
           nextButton.setBounds((int)origin.getX() - 70, maxY + 30, 40, 20);
@@ -202,6 +173,8 @@ public class RealDrawing extends JPanel{
           speedSlider.setPaintTicks(true);
           speedSlider.setPaintLabels(true);
           
+          //Adding a changeListener to the slider so that it changes the speed when the user moves the slider.
+          
           speedSlider.addChangeListener(new ChangeListener() { 
         	  @Override
         	    public void stateChanged(ChangeEvent e) {
@@ -215,12 +188,19 @@ public class RealDrawing extends JPanel{
           
           speedSlider.setBounds((int)f.getSize().getWidth() - 400, 60, 300,40);
           
+          //Labelling the slider.
           JLabel sliderLabel = new JLabel("Speed");
           sliderLabel.setBounds((int)f.getSize().getWidth() - 275,30,100,30);
           
+          
+          //These buttons are the buttons in the top left corner which clear the board, add a big hexagon, and add the line across the middle.
           JButton bigHexButton = new JButton();
           bigHexButton.setBounds(100, 100, 150, 30);
           bigHexButton.setText("Big Hexagon");
+          
+          JButton lineButton = new JButton();
+          lineButton.setBounds(100,150,150,30);
+          lineButton.setText("Line");
           
           JButton clearButton = new JButton();
           clearButton.setBounds(100,50,150,30);
@@ -232,10 +212,13 @@ public class RealDrawing extends JPanel{
           f.add(speedSlider);
           f.add(sliderLabel);
           f.add(bigHexButton);
+          f.add(lineButton);
           f.add(clearButton);
           
           f.setLocationRelativeTo(null);
           f.setVisible(true);
+          
+          //Adding a MouseListener to the buttons below the grid (next, start, and end)
           
           nextButton.addMouseListener(new MouseListener() {
 			@Override
@@ -342,6 +325,10 @@ public class RealDrawing extends JPanel{
         			
         		}
     		});
+          
+          
+          //Adding MouseListeners to the buttons in the top left (adding the big hexagon or the line, and clearing the board)
+          
           bigHexButton.addMouseListener(new MouseListener() {
     			@Override
         		public void mouseClicked(MouseEvent e) {
@@ -395,6 +382,41 @@ public class RealDrawing extends JPanel{
         			
         		}
     		});
+           lineButton.addMouseListener(new MouseListener() {
+  			@Override
+      		public void mouseClicked(MouseEvent e) {
+      			if (lineButton.contains(e.getPoint())) {
+      				for(int k = 0; k < 50; k++) {
+      					grid[k][25].toggle();
+      				}
+      				f.repaint();
+      			}
+      		}
+
+     			@Override
+     			public void mousePressed(MouseEvent e) {
+     				// TODO Auto-generated method stub
+     				
+     			}
+
+      		@Override
+      		public void mouseReleased(MouseEvent e) {
+      			// TODO Auto-generated method stub
+      			
+      		}
+
+      		@Override
+      		public void mouseEntered(MouseEvent e) {
+      			// TODO Auto-generated method stub
+      			
+      		}
+
+      		@Override
+      		public void mouseExited(MouseEvent e) {
+      			// TODO Auto-generated method stub
+      			
+      		}
+  		});
             clearButton.addMouseListener(new MouseListener() {
     			@Override
         		public void mouseClicked(MouseEvent e) {
@@ -436,21 +458,21 @@ public class RealDrawing extends JPanel{
     		});
 	}
 	
+	
+	//Iterates the grid once without a delay -- used when the "Next" button is clicked and for the first iteration after pressing "Start"
 	public static void iterateNoDelay(Buttons[][] hexGrid, JFrame f) {
 		int size = hexGrid.length;
-		boolean[][] newTurnedOn = new boolean[size][size];
+		boolean[][] newTurnedOn = new boolean[size][size]; // creating the next iteration
 		for(int i = 0; i < size; i++) {
 			for(int j = 0; j < size; j++) {
 	            if(hexGrid[i][j] == null) {
-	            	//System.out.println("radius 0");
+	            	//System.out.println("radius 0"); (To avoid null pointer exception)
 	            }
-	            else if(i==0||i==(size-1)||j==0||j==(size-1)||i+j==size/2||i+j==size+size/2) {
+	            else if(i==0||i==(size-1)||j==0||j==(size-1)||i+j==size/2||i+j==size+size/2-1) {
 	            	//this checks borders
-	            	//System.out.println("border");
 	            }
 	            else {
-	            	//System.out.println("hello");
-	            	int count = 0;
+	            	int count = 0; //number of hexagons surrounding a given hexagon that are on
 	            	if(hexGrid[i][j-1] != null && hexGrid[i][j-1].isOn()) {
 	            		count++;
 	            	}
@@ -470,20 +492,16 @@ public class RealDrawing extends JPanel{
 	            		count++;
 	            	}
 	            	
-	            	//rules implemented below
-	            	//System.out.println(count);
+	            	//Rules implemented below
 	            	if(hexGrid[i][j].isOn()) {
 	            		
 	            		if(!(count==2)&&!(count==3)) {
 	            			newTurnedOn[i][j]=false;
-	            			//System.out.println("something been turned off");
 	            		}
 	            	}
 	            	else if(!hexGrid[i][j].isOn()) {
 	            		if(count==2)  {
 	            			newTurnedOn[i][j]=true;
-	            			//System.out.println(p.getHexGrid()[i][j].getCenter());
-	            			//System.out.println("Hexagon "+i+","+j+" been turn on");
 	            		}
 	            	}
 	            }
@@ -495,30 +513,28 @@ public class RealDrawing extends JPanel{
 				if(hexGrid[i][j] != null && hexGrid[i][j].getRadius()!=0) {
 					if(hexGrid[i][j].isOn()!=newTurnedOn[i][j]) {
 	            		hexGrid[i][j].toggle();
-	            		//System.out.println("Hexagon at "+i+", "+j+" has been toggled");
 	            	}
 				}
 			}
 		}
 		f.repaint();
 	}
-
+	
+	//Same code as iterateNoDelay, except this adds a delay based on the position of the slider after iterating once.
+	//This is used for each iteration after the first during the loop between pressing "Start" and "End".
 	public static void iterate(Buttons[][] hexGrid, JFrame f) {
-		
 		int size = hexGrid.length;
-		boolean[][] newTurnedOn = new boolean[size][size];
+		boolean[][] newTurnedOn = new boolean[size][size]; // creating the next iteration
 		for(int i = 0; i < size; i++) {
 			for(int j = 0; j < size; j++) {
 	            if(hexGrid[i][j] == null) {
-	            	//System.out.println("radius 0");
+	            	//System.out.println("radius 0"); (To avoid null pointer exception)
 	            }
-	            else if(i==0||i==(size-1)||j==0||j==(size-1)||i+j==size/2||i+j==size+size/2) {
+	            else if(i==0||i==(size-1)||j==0||j==(size-1)||i+j==size/2||i+j==size+size/2-1) {
 	            	//this checks borders
-	            	//System.out.println("border");
 	            }
 	            else {
-	            	//System.out.println("hello");
-	            	int count = 0;
+	            	int count = 0; //number of hexagons surrounding a given hexagon that are on
 	            	if(hexGrid[i][j-1] != null && hexGrid[i][j-1].isOn()) {
 	            		count++;
 	            	}
@@ -537,20 +553,17 @@ public class RealDrawing extends JPanel{
 	            	if(hexGrid[i+1][j-1] != null && hexGrid[i+1][j-1].isOn()) {
 	            		count++;
 	            	}
-	            	//rules implemented below
-	            	//System.out.println(count);
+	            	
+	            	//Rules implemented below
 	            	if(hexGrid[i][j].isOn()) {
 	            		
 	            		if(!(count==2)&&!(count==3)) {
 	            			newTurnedOn[i][j]=false;
-	            			//System.out.println("something been turned off");
 	            		}
 	            	}
 	            	else if(!hexGrid[i][j].isOn()) {
 	            		if(count==2)  {
 	            			newTurnedOn[i][j]=true;
-	            			//System.out.println(p.getHexGrid()[i][j].getCenter());
-	            			//System.out.println("Hexagon "+i+","+j+" been turn on");
 	            		}
 	            	}
 	            }
@@ -562,14 +575,15 @@ public class RealDrawing extends JPanel{
 				if(hexGrid[i][j] != null && hexGrid[i][j].getRadius()!=0) {
 					if(hexGrid[i][j].isOn()!=newTurnedOn[i][j]) {
 	            		hexGrid[i][j].toggle();
-	            		//System.out.println("Hexagon at "+i+", "+j+" has been toggled");
 	            	}
 				}
 			}
 		}
 		f.repaint();
+		
+		//Creates the delay after iterating.
 		try{
-			Thread.sleep(100*(11 - speed));
+			Thread.sleep(1000 / speed);
 			//System.out.println("sleep");
 			}catch(InterruptedException ex) {
 				System.out.println("no");
